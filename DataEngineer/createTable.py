a820b7bf507ae3742709a7c6f3b0ec5c2f020978
Bruno Gurgel <146783585+code-2go@users.noreply.github.com>
2024-10-18 15:22:37 -0300

    Update createTable.py
    New parameters have been added

--- DataEngineer/createTable.py
+++ DataEngineer/createTable.py
@@ -5,7 +5,7 @@ import sqlite3
 dbName = 'SalesHistory.db'
 tableName = 'Sales2024'
 conn = sqlite3.connect(dbName)
-conn.execute(f'CREATE TABLE {tableName} (Product1, Quantity, Value)')
+conn.execute(f'CREATE TABLE {tableName} (Id_pedido, Data, Id_Cliente, Fornecedor, Cod_Produto, Qtd., Vlr Uni, Vlr IPI, Vlr Tot, Vlr Tot IPI)')
 
 conn.commit
 conn.close
