05d3ff687b1b0564c74893a0f37d25d28fd35003
Bruno <bgurgelamaral@gmail.com>
2024-10-21 10:15:27 -0300

    Inserted explanations for each important line of the script

--- DataEngineer/readCSV.py
+++ DataEngineer/readCSV.py
@@ -2,20 +2,21 @@ import csv
 import sqlite3
 
 dbname = 'SalesHistory.db'
-csvfile ="histórico de pedido 2024"
+csvfile ="histórico de pedidos 2024"
 tableName = 'sales2024'
-with open (csvfile, 'r') as file:
-    reader = csv.reader(file)
-    next(reader)
-    conn = sqlite3.connect(dbname)
 
-    for row in reader:
+with open (csvfile, 'r') as file:
+    reader = csv.reader(file) # Leitura do arquivo csv
+    next(reader) # Pula a primeira linha (header) do arquivo
+    conn = sqlite3.connect(dbname) # Conexão ao banco de dados
+
+    for row in reader: # Looping para inserção de dados na tabela
         conn.execute(f'''
                     INSERT INTO {tableName} 
                     (ID_Pedido, Data, ID_Cliente, Fornecedor, Qtd., Vlr.Unit, Vlr.IPI, Vlr.Tot., Vlr.Tot.IPI)
                     Values (?,?,?,?,?,?,?,?,?,?)
                      ''')
-conn.commit()
-conn.close()
+conn.commit() 
+conn.close() #Fechamento da conexão, poupando recursos do sistema
 
-print('The file has been processed')
+print('The file has been processed') 
