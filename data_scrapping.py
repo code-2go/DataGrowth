75e01689b9e23f9bd2a76447133a1f53dbb68e50
Bruno <bgurgelamaral@gmail.com>
2024-11-28 15:19:48 -0300

    Duplicate files have been removed

--- data_scrapping.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def pull_data(URL_source, table):
-    '''Returns a specific table from the web page where the first argument is the URL and the second is the web page table'''
-
-    df = pd.read_html(URL_source)
-    return df[table]
